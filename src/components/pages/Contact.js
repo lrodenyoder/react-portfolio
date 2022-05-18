66aa2b67a374598ee46c5b20125b4072b98a757a
lrodenyoder <rodenlau000@gmail.com>
2022-05-17 19:55:40 -0500

    set up contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,39 @@
-import React from "react";
+import { validateEmail } from "../../utils/helpers";
+import React, { useState } from "react";
 
 function Contact() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+
+      if (!isValid) {
+        setErrorMessage("Your email address is invalid");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+
+    console.log("errorMessage", errorMessage);
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
   return <div>Contact Here</div>;
 }
 
