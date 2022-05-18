6bd77ab972cbc68b0fe6eb9199d46e607c3ac076
lrodenyoder <rodenlau000@gmail.com>
2022-05-17 19:58:55 -0500

    add contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -33,8 +33,55 @@ function Contact() {
       setFormState({ ...formState, [e.target.name]: e.target.value });
     }
   }
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
 
-  return <div>Contact Here</div>;
+  return (
+    <section className='mx-2'>
+      <h1>Contact Me</h1>
+      <form id="contact-form">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="text"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onBlur={handleChange}
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        {/* && is used as a short circuit. if the first value resolves to true, the second value will be evaluated */}
+        <button type="submit" onSubmit={handleSubmit} data-testid="submit-btn">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
 }
 
 export default Contact;
