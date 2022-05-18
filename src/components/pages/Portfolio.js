9c4d63d91d8e006829c4bfa62625250f7ec26c38
lrodenyoder <rodenlau000@gmail.com>
2022-05-18 17:56:19 -0500

    fix bootstrap conflicts

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,47 +2,6 @@ import React from "react";
 
 function Portfolio() {
   return (
-    // <div className="row row-cols-1 row-cols-sm-2 g-3">
-    //   <div className="col">
-    //     <div className="card bg-dark text-dark">
-    //       <img
-    //         src={require("../../assets/homebuddy-screenshot.jpg")}
-    //         alt="Homebuddy screenshot"
-    //         className="card-img"
-    //       ></img>
-    //       <div className="card-img-overlay">
-    //         <h5 className="card-title">Project 1</h5>
-    //         <p className="card-text">Project Text</p>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className="col">
-    //     <div className="card bg-dark text-dark">
-    //       <img
-    //         src={require("../../assets/nutrigainz-screenshot.jpg")}
-    //         alt="Homebuddy screenshot"
-    //         className="card-img"
-    //       ></img>
-    //       <div className="card-img-overlay">
-    //         <h5 className="card-title">Project 1</h5>
-    //         <p className="card-text">Project Text</p>
-    //       </div>
-    //     </div>{" "}
-    //   </div>
-    //   <div className="col">
-    //     <div className="card bg-dark text-dark">
-    //       <img
-    //         src={require("../../assets/run-buddy-screenshot.jpg")}
-    //         alt="Homebuddy screenshot"
-    //         className="card-img"
-    //       ></img>
-    //       <div className="card-img-overlay">
-    //         <h5 className="card-title">Project 1</h5>
-    //         <p className="card-text">Project Text</p>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3">
       <div className="col my-1">
         <div className="card">
@@ -85,6 +44,6 @@ function Portfolio() {
       </div>
     </div>
   );
-}
+};
 
 export default Portfolio;
